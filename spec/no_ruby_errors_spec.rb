6268005e7a1cb7b018ecb800f7a92f268cbe0e51
Prabhjot Monder <prabhlem@gmail.com>
2018-03-15 18:13:39 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -25,7 +25,7 @@ describe "Not having any errors and being all green" do
   end
 
   context 'ZeroDivisionError' do
-    it 'raises a ZeroDivisionError for dividing by zero' do
+    puts 'raises a ZeroDivisionError for dividing by zero' do
       expect{
         load './lib/a_division_by_zero_error.rb'
       }.to_not raise_error
